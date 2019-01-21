7fa2f52736d08a89a31ae7296209d6f005513815
HaileyP <hdpolson@att.net>
2019-01-21 13:47:07 -0600

    Added Ultrasonic Sensor code

--- Arduino/Ultrasonic_Maxbotix_MB1013/Ultrasonic_Sensor/Ultrasonic_Sensor.ino
+++ Arduino/Ultrasonic_Maxbotix_MB1013/Ultrasonic_Sensor/Ultrasonic_Sensor.ino
@@ -1,25 +1,35 @@
 
-const int anPin = 0;
-long anVolt, cm;
+#include "ultrasonicAPI.h"
+
+//const int anPin = 0;
+//long anVolt, cm;
+
 
 void setup() {
   Serial.begin(9600);
 }
 
-void read_sensor(){
-  anVolt = analogRead(anPin);
-  cm = anVolt/2;
-}
+//void read_sensor(){
+//  anVolt = analogRead(anPin);
+//  cm = anVolt/2;
+//}
+
+//long getDistanceUsingUltrasonic() {
+//const ulstrasonicPin = 0;
+//  long voltageRead = analogRead(ulstrasonicPin);
+//  long distanceInCentimeters = ulstrasonicPin / 2;
+//  return distanceInCentimeters;
+//}
 
 void print_range(){
   Serial.print("Range = ");
-  Serial.print(cm);
+  Serial.print(getDistanceUsingUltrasonic());
   Serial.print(" cm ");
   Serial.print('\n');
 }
 
 void loop() {
-  read_sensor();
+//  read_sensor();
   print_range();
   delay(100);
 }
