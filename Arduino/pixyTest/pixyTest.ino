d3d1f8afe0dada9aac954ce1648609025d818c02
HaileyP <hdpolson@att.net>
2019-02-08 22:52:42 -0600

    Added percentages to pixy cam line follower

--- Arduino/pixyTest/pixyTest.ino
+++ Arduino/pixyTest/pixyTest.ino
@@ -8,44 +8,20 @@ void setup() {
   pixy2.init();
 }
 
-//Prints YOUR location in refrence to the Line
+//Prints your location in percentages left or right of the line
 
-void loop() {
+float getLineRefrence() {
+  float lineLocation, percentage;
   // put your main code here, to run repeatedly:
   pixy2.ccc.getBlocks();
   
-  
-  if (pixy2.ccc.blocks[0].m_x > 0 && pixy2.ccc.blocks[0].m_x < 50) {
-    digitalWrite(6, HIGH); //turn left a lot
-    Serial.print("1 1/4 in to the left");
-    Serial.print('\n');
-    delay(500);
-  } 
-  if(pixy2.ccc.blocks[0].m_x > 50 && pixy2.ccc.blocks[0].m_x < 125) {
-    digitalWrite(7, HIGH); //turn left a little
-    Serial.print("1/2 in to the left");
-    Serial.print('\n');
-    delay(500);
-  }
-  if (pixy2.ccc.blocks[0].m_x > 125 && pixy2.ccc.blocks[0].m_x < 175) {
-    digitalWrite(6, HIGH); 
-    digitalWrite (7, HIGH);
-    digitalWrite(8, HIGH); 
-    digitalWrite (9, HIGH); //go straight
-    Serial.print("straight");
-    Serial.print('\n');
-    delay(500);
-  }
-  if (pixy2.ccc.blocks[0].m_x > 175 && pixy2.ccc.blocks[0].m_x < 250) {
-    digitalWrite(8, HIGH); //turn right a little
-    Serial.print("1/2 in to the right");
-    Serial.print('\n');
-    delay(500);
-  }
-  if (pixy2.ccc.blocks[0].m_x > 250 && pixy2.ccc.blocks[0].m_x < 300) {
-    digitalWrite(9, HIGH); //turn right a lot
-    Serial.print("1 1/4 in to the right");
-    Serial.print('\n');
-    delay(500);
-  }
+  lineLocation = pixy2.ccc.blocks[0].m_x;
+  percentage = (lineLocation - 157.5)/157.5;
+  return percentage;
+}
+
+void loop() {
+  Serial.print(getLineRefrence());
+  Serial.print("%");
+  Serial.print("\n");
 }
